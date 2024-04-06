1806124f20c010ce0f4773f4db460c690a818a24
ninq4 <nik.dolgov.05@mail.ru>
2024-04-06 10:42:58 +0300

    pre

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Roboto_Mono } from "next/font/google";
+import "@/styles/main.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+const robotoMono = Roboto_Mono({ subsets: ["cyrillic"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={robotoMono.className}>{children}</body>
     </html>
   );
 }
